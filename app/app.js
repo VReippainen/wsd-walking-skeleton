3f66a92c166cfd947337b562465e44fd02df780b
Ville Reippainen <ville.reippainen@wunderdog.fi>
2022-04-10 13:38:29 +0300

    Add Procfile for Heroku

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
 
 const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  return new Response("Hello world!");
+  const url = new URL(request.url);
+  const params = url.searchParams;
+
+  const one = Number(params.get("one"));
+  const two = Number(params.get("two"));
+
+  return new Response(`Sum: ${one + two}`);
 };
 
-console.log("Launching server on port 7777");
-serve(handleRequest, { port: 7777 });
+let port = 7777;
+if (Deno.args.length > 0) {
+  const lastArgument = Deno.args[Deno.args.length - 1];
+  port = Number(lastArgument);
+}
+
+serve(handleRequest, { port: port });
